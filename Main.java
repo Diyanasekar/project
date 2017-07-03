f2583f266dcce851654b2ee73d14551115480794
Diyanasekar <diyanas1997@gmail.com>
2017-07-03 09:41:52 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+# project
+import jav.io.*;
+import java.util.*;
+public class Main
+{
+public static void mani(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n;
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("even);
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
